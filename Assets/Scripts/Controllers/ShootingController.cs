67cdf7c328a294c9cee09059beafbc8a95c09d60
[Ivan Filippov] <ifilippov2020@gmail.com>
2021-12-05 23:45:43 +0300

    Починил рэйкаст

--- Assets/Scripts/Controllers/ShootingController.cs
+++ Assets/Scripts/Controllers/ShootingController.cs
@@ -9,39 +9,41 @@ namespace AsteroidS
 {
     internal class ShootingController : IInitialization, IExecute
     {
+        private GameData _gameData;
         private Transform _player;
-        private float _shotDistance = 50f;
-        private float _rateOfFire = 3;
+        private float _shotDistance;
+        private float _rateOfFire;
         private float _bulletRemovalTimer = 0.5f;
         private float _speedRate=1;
         private Ammo _ammo;
-        private Vector3 _shotOffset;
+        //private Vector3 _shotOffset;
         private bool _shootingTimer = true;
         private Coroutine _coroutineTimer;
-        private Coroutine _counterTimerDelet;
+        private Coroutine _counterTimerDelete;
 
 
         internal ShootingController(GameData gameData, Transform player)
         {
+            _gameData = gameData;
             _player = player;
-            //_rateOfFire = gameData.PlayerData.currentAmmo.Properties.fireRate;
-            //_shotDistance = gameData.PlayerData.currentAmmo.Properties.shotDistance;
-            //_speedRate = gameData.PlayerData.currentAmmo.Properties.speedRate;
-            _ammo = gameData.PlayerData.currentAmmo;
-            _shotOffset = gameData.PlayerData.ShotOffset;
         }
 
         public void Initialize()
         {
-
+            _rateOfFire = _gameData.PlayerData.currentAmmo.Properties.fireRate;
+            _shotDistance = _gameData.PlayerData.currentAmmo.Properties.shotDistance;
+            _speedRate = _gameData.PlayerData.currentAmmo.Properties.speedRate;
+            _ammo = _gameData.PlayerData.currentAmmo;
+            //_shotOffset = _gameData.PlayerData.ShotOffset;
         }
 
 		public void Execute(float deltaTime)
 		{
-            int layerMaskOnlyPlayer = 1 << 8;
-            int layerMaskWithoutPlayer = ~layerMaskOnlyPlayer;
-            var hit = Physics2D.Raycast(_player.position, _player.eulerAngles, _shotDistance, layerMaskWithoutPlayer);
-            Debug.DrawRay(_player.position, _player.eulerAngles, Color.red);
+            int mask = LayerMask.GetMask(MasksHolder.SpaceObject);
+
+            var hit = Physics2D.Raycast(_player.position, _player.up, _shotDistance, mask);
+            Debug.DrawRay(_player.position, _player.up*_shotDistance, Color.red);
+            
             if (hit == _ammo)
 			{
 				if (_shootingTimer == true)
@@ -67,14 +69,14 @@ namespace AsteroidS
             var rb = shot.GetComponent<Rigidbody2D>();
             var vector = _player.localRotation.eulerAngles.normalized;
             rb.AddForce(vector * _speedRate);
-            _counterTimerDelet = CoroutinesController.StartRoutine(TimerDelet(_bulletRemovalTimer,shot));
+            _counterTimerDelete = CoroutinesController.StartRoutine(TimerDelete(_bulletRemovalTimer,shot));
         }
 
-        IEnumerator TimerDelet(float timeInSec, GameObject shot)
+        IEnumerator TimerDelete(float timeInSec, GameObject shot)
         {
             yield return new WaitForSeconds(timeInSec);
             Object.Destroy(shot);
-            CoroutinesController.StopRoutine(_counterTimerDelet);
+            CoroutinesController.StopRoutine(_counterTimerDelete);
         }
 
 	}
---
+++ Assets/Scripts/Data/MasksHolder.cs
@@ -0,0 +1,8 @@
+﻿namespace AsteroidS
+{
+    public static class MasksHolder
+    {
+        public const string Default = "Default";
+        public const string SpaceObject = "SpaceObject";
+    }
+}
