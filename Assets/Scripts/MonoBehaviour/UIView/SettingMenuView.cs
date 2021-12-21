ee264ea614293cac6202130a43edba0b15fbcc96
Brundewald <77693457+Brundewald@users.noreply.github.com>
2021-12-21 14:08:22 +0300

    Revert "Merge pull request #18 from iFilippov1983/StableUI"
    This reverts commit c824e62d03743b511b69d503060993ba81e89317, reversing
    changes made to 8189a82ce83bcfa7486635411ba3a3617d758c5f.

--- Assets/Scripts/MonoBehaviour/UIView/SettingMenuView.cs
+++ Assets/Scripts/MonoBehaviour/UIView/SettingMenuView.cs
@@ -13,7 +13,6 @@ namespace AsteroidS
         [SerializeField] private Slider _volumeSlider;
         [SerializeField] private DropdownMenu _graphicsDropdown;
 
-        
         public Image BackgroundImage => _backgroundImage;
         public Button BackButton => _backButton;
         public Slider VolumeSlider => _volumeSlider;
