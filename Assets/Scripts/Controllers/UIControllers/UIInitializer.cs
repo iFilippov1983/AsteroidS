9491af903c323c1197f03d04cd8fb5124a693ad9
Brundewald <77693457+Brundewald@users.noreply.github.com>
2021-12-21 14:35:04 +0300

    Revert "Revert "Восстановление""
    This reverts commit 855bccd350ec36b8f2642798851b11f38a91a420.

--- Assets/Scripts/Controllers/UIControllers/UIInitializer.cs
+++ Assets/Scripts/Controllers/UIControllers/UIInitializer.cs
@@ -1,31 +1,32 @@
-﻿using UnityEngine;
+﻿using AsteroidS.UIView;
+using UnityEngine;
 
 namespace AsteroidS
 {
     public class UIInitializer
     {
+<<<<<<< HEAD
         private readonly GameObject _mainMenuPrefab;
         private readonly GameObject _settingsMenuPrefab;
         private readonly GameObject _playerUIPrefab;
         private readonly GameObject _deathScreenPrefab;
         
+=======
+        private GameObject _mainMenuPrefab;
+        private GameObject _settingsMenuPrefab;
+        private GameObject _playerUIPrefab;
+>>>>>>> parent of 778962c4 (Merge branch 'UI_by_Nikita_M' into IvanF_work_branch2)
         private GameObject _uiRoot;
         private GameObject _mainMenu;
         private GameObject _settingsMenu;
         private GameObject _playerUI;
-        private GameObject _deathScreen;
-        
-        public GameObject PlayerUI => _playerUI;
-        public GameObject MainMenu => _mainMenu;
-        public GameObject SettingsMenu => _settingsMenu;
-        public GameObject DeathScreen => _deathScreen;
+
 
         public UIInitializer(GameData gameData)
         {
             _mainMenuPrefab = gameData.UIData.MainMenu;
             _settingsMenuPrefab = gameData.UIData.SettingsMenu;
             _playerUIPrefab = gameData.UIData.PlayerUI;
-            _deathScreenPrefab = gameData.UIData.DeathScreen;
             CreateUI();
         }
 
@@ -37,7 +38,6 @@ namespace AsteroidS
                 _mainMenu = GetMainMenu();
                 _playerUI = GetPlayerUI();
                 _settingsMenu = GetSettingsMenu();
-                _deathScreen = GetDeathScreen();
             }
         }
 
@@ -59,10 +59,8 @@ namespace AsteroidS
             return settingsMenu;
         }
 
-        private GameObject GetDeathScreen()
-        {
-            var deathScreen = Object.Instantiate(_deathScreenPrefab, _uiRoot.transform);
-            return deathScreen;
-        }
+        public GameObject PlayerUI => _playerUI;
+        public GameObject MainMenu => _mainMenu;
+        public GameObject SettingsMenu => _settingsMenu;
     }
 }
